83080b28c6d9275645a7f9e0836f4f53935f009b
Ktyal Plummer <kp7546@cs.ship.edu>
2022-10-03 15:20:37 +0000

    Revert "adding acidTDG"
    This reverts commit a93b292206bfb258ee6cc97f9f6daa18eceed326

---
+++ src/main/gatewayDTOs/MetalDTO.java
@@ -0,0 +1,64 @@
+package gatewayDTOs;
+
+public class MetalDTO {
+   private  long id;
+   private   String name;
+   private   long atomicNumber;
+   private   double atomicMass;
+
+   public MetalDTO(long id, String name, long atomicNumber, long atomicMass){
+
+      this.id = id;
+      this.name = name;
+      this.atomicNumber = atomicNumber;
+      this.atomicMass = atomicMass;
+   }
+
+   public void setId(long id) {
+      this.id = id;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public void setAtomicNumber(long atomicNumber) {
+      this.atomicNumber = atomicNumber;
+   }
+
+   public void setAtomicMass(double atomicMass) {
+      this.atomicMass = atomicMass;
+   }
+
+   /**
+    *
+    * @return the ID
+    */
+   public long getId() {
+      return id;
+   }
+
+   /**
+    *
+    * @return the name of the metal
+    */
+   public String getName() {
+      return name;
+   }
+
+   /**
+    *
+    * @return the atomic number
+    */
+   public long getAtomicNumber() {
+      return atomicNumber;
+   }
+
+   /**
+    *
+    * @return the atomicmass
+    */
+   public double getAtomicMass() {
+      return atomicMass;
+   }
+}
