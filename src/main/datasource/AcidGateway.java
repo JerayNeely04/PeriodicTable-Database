d409de5c99635316a1c529dc1ff1c1e0690ffcf2
Jeray Neely-Speaks <jn3557@engr.ship.edu>
2022-10-05 18:14:47 +0000

    Update AcidGateway

--- src/main/datasource/AcidGateway.java
+++ src/main/datasource/AcidGateway.java
@@ -1,6 +1,9 @@
 package datasource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class AcidGateway {
@@ -9,7 +12,121 @@ public class AcidGateway {
     private long solute;
     private final Connection connection;
 
-    public AcidGateway(String name, long solute) {
+    public AcidGateway(String name, long solute)throws DataException{
+
         this.connection = DatabaseConnection.getInstance().getConnection();
+        this.name = name;
+        this.solute =solute;
+        this.createRow();
     }
+
+    public AcidGateway(long id)throws DataException{
+        this.connection = DatabaseConnection.getInstance().getConnection();
+        String query = "SELECT * FROM AcidTable WHERE id = " + id;
+
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            ResultSet results = stmt.executeQuery();
+            results.next();
+
+            this.id = id;
+            this.name = results.getString("name");
+            this.solute = results.getLong("solute");
+        } catch (SQLException e) {
+            throw new DataException("Could not find base with ID: " +  id, e);
+        }
+
+    }
+
+    /**
+     * creates a new role values id,name, and solute
+     * @throws DataException database exception
+     */
+    private void createRow() throws DataException {
+
+        String query = "INSERT INTO AcidTable (id,name,solute) VALUES (?,?,?)";
+
+        try{
+            long id = KeyRowDataGateway.generateId();
+            this.id = id;
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setLong(1, id);
+            stmt.setString(2, name);
+            stmt.setLong(3, solute);
+            stmt.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new DataException("Create Acid row failed", e);
+        }
+    }
+
+    /**
+     *
+     * @return updates the row in the acid table.
+     * return true if the row was updated, if not return false.
+     * @throws DataException
+     */
+    public boolean update() throws DataException {
+        String query = "UPDATE AcidTable SET name = ?, " +
+                "solute = ? " +
+                "WHERE id = " + id;
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, name);
+            stmt.setLong(2, solute);
+
+            if (stmt.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new DataException("Acid row update failed", e);
+        }
+
+        return false;
+    }
+
+    /**
+     *  deletes the row that was inserted into the database.
+     * @throws DataException
+     */
+    public void delete() throws DataException {
+        String query = "DELETE FROM AcidTable WHERE id = " + id;
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataException("Acid row could not be deleted", e);
+        }
+    }
+
+    /**
+     * creates a new Acid table within the database.
+     * @throws DataException
+     */
+    public static void createTable() throws DataException {
+        Connection conn = DatabaseConnection.getInstance().getConnection();
+        String dropStatement = "DROP TABLE IF EXISTS AcidTable";
+        String createStatement = "CREATE TABLE AcidTable (" +
+                "id BIGINT PRIMARY KEY, " +
+                "name VARCHAR(40), " +
+                "solute BIGINT)";
+
+        try {
+            PreparedStatement stmt;
+            stmt = conn.prepareStatement(dropStatement);
+            stmt.execute();
+            stmt.close();
+
+            stmt = conn.prepareStatement(createStatement);
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new DataException("Could not create Acid table", e);
+        }
+    }
+
+
 }
