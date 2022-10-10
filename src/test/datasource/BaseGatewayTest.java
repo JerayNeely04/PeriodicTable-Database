7ae3d30a3fce1ff3202b85bd81b326112f817e91
Ktyal Plummer <ktyalplummer@gmail.com>
2022-10-10 16:57:34 -0400

    base gateway tests fixed

--- src/test/datasource/BaseGatewayTest.java
+++ src/test/datasource/BaseGatewayTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class BaseGatewayTest {
-    Connection conn = DatabaseConnection.getInstance().getConnection();
+    private Connection conn = DatabaseConnection.getInstance().getConnection();
 
     /**
      * Tests to make sure a base can be created
