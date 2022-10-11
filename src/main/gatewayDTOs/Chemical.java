c43b8e6a0034f6ad10d008d4b2a8df8b0db6d9bc
Badreddine <bb6848@ship.edu>
2022-10-11 13:32:08 -0400

    Added javadocs to the Chemical and Element DTO

--- src/main/gatewayDTOs/Chemical.java
+++ src/main/gatewayDTOs/Chemical.java
@@ -1,26 +1,49 @@
 package gatewayDTOs;
 
+/**
+ * Chemical Dto for the Chemical gateway
+ */
 public class Chemical {
     private long id;
     private String name;
 
+    /**
+     * Chemical constructor
+     * @param id is the primary key for the chemical Table
+     * @param name name of the Chemical
+     */
     public Chemical(long id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * getter for the ID
+     * @return chemical ID
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * get the name of the chemical
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter for the ID
+     * @param id
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Setter for the name of the chemical
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
--- src/main/gatewayDTOs/Element.java
+++ src/main/gatewayDTOs/Element.java
@@ -1,26 +1,50 @@
 package gatewayDTOs;
 
+/**
+ * Element DTO for the Element gateway
+ */
 public class Element {
     private long atomicNumber;
     private double atomicMass;
 
+    /**
+     * Constructor for the Element DTO
+     * @param atomicNumber is the primary key
+     * @param atomicMass
+     */
     public Element(long atomicNumber, double atomicMass) {
         this.atomicNumber = atomicNumber;
         this.atomicMass = atomicMass;
     }
 
+    /**
+     * Getter for the Atomic number
+     * @return atomic number of the element
+     */
     public long getAtomicNumber() {
         return atomicNumber;
     }
 
+    /**
+     * Setter for the Atomic number
+     * @param atomicNumber
+     */
     public void setAtomicNumber(long atomicNumber) {
         this.atomicNumber = atomicNumber;
     }
 
+    /**
+     * Getter for thr atomic mass
+     * @return the atomic mass of the element
+     */
     public double getAtomicMass() {
         return atomicMass;
     }
 
+    /**
+     * Setter for the atomic mass
+     * @param atomicMass
+     */
     public void setAtomicMass(double atomicMass) {
         this.atomicMass = atomicMass;
     }
